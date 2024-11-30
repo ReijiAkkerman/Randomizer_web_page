90b2c758d05b61faffb3c119ae8703f50ba8c70f
ReijiAkkerman <reijiakkerman@gmail.com>
2024-11-30 18:30:49 +0300

    тест свайпа

--- src/js/mobile/Swipes.js
+++ src/js/mobile/Swipes.js
@@ -1,171 +1,118 @@
 import {Words} from '/src/js/MainFrame/Words.js';
 import {Settings} from '/src/js/MainFrame/Settings.js';
-//import {Guide} from '/src/js/MainFrame/Guide.js';
+
+/** Описание функций */
+// сamelCase для разрешений
+// snake_notation для обработки самого свайпа
 
 class Swipes {
     static selectors = new Map([
         ['Навигационная панель', '.dashboard'],
         ['Скролл', 'main'],
+        ['', ''],
     ]);
 
     static dashboard = document.querySelector(Swipes.selectors.get('Навигационная панель'));
-
-    /** Работа */
+    static main = document.querySelector(Swipes.selectors.get('Скролл'));
 
     static #panels = new Map([
         [0, Swipes.dashboard],
         [1, Words.main_section],
         [2, Settings.main_section],
-        // [3, Guide.main_section],
     ]);
 
-    static show_permission = false;
-    static #change_panel_number_permission = true;
     static #startX;
     static #startY;
     static #currentX;
     static #currentY;
     static #deltaX;
     static #deltaY;
-
-    static #current_panel_number = 1;
-    static #previous_panel_number = 1;
-
-    static permit_right_panel_opening = false;
-    static permit_left_panel_opening = true;
-    static switching_permitted = true;
-
-    
+    static direction;
+    static #panel_number = 1;
+    static panel_edge = 'left';
 
 
 
-    /**
-     * Функции для работы непосредственно свайпа
-     */
 
-    static show_target_panel() {
-        let panel = Swipes.#panels.get(Swipes.#current_panel_number);
-        panel.style.display = '';
-        Swipes.#previous_panel_number = Swipes.#current_panel_number;
-        Swipes.#reset_positions();
-    }
 
-    static hide_current_panel() {
-        let panel = Swipes.#panels.get(Swipes.#previous_panel_number);
-        panel.style.display = 'none';
-    }
 
-    static get_initial_position(event) {
+
+    static set_initial_coordinates(event) {
         Swipes.#startX = event.touches[0].clientX;
         Swipes.#startY = event.touches[0].clientY;
     }
 
-    static get_current_position(event) {
+    static get_current_coordinates(event) {
         Swipes.#currentX = event.touches[0].clientX;
         Swipes.#currentY = event.touches[0].clientY;
+        Swipes.#deltaX = Swipes.#startX - Swipes.#currentX;
+        Swipes.#deltaY = Swipes.#startY - Swipes.#currentY;
     }
 
-    static define_show_permission() {
-        Swipes.#deltaX = Swipes.#currentX - Swipes.#startX;
-        Swipes.#deltaY = Swipes.#currentY - Swipes.#startY;
-        let deltaX = Swipes.#toPositive(Swipes.#deltaX);
-        let deltaY = Swipes.#toPositive(Swipes.#deltaY);
-        label_if: {
-            if(deltaX > 40) {
-                if(deltaY < 40) {
-                    if(Swipes.#change_panel_number_permission) {
-                        if(Swipes.#deltaX > 0)
-                            if(Swipes.permit_left_panel_opening && Swipes.switching_permitted)
-                                Swipes.#current_panel_number = --Swipes.#current_panel_number < 0 ? Swipes.#panels.size - 1 : Swipes.#current_panel_number;
-                            else 
-                                break label_if;
-                        else 
-                            if(Swipes.permit_right_panel_opening && Swipes.switching_permitted)
-                                Swipes.#current_panel_number = ++Swipes.#current_panel_number % Swipes.#panels.size;
-                            else 
-                                break label_if;
-                        Swipes.show_permission = true;
-                        Swipes.#change_panel_number_permission = false;
-                    }
-                }
-            }
+    static define_direction() {
+        if(Swipes.#deltaX > 0) Swipes.direction = 'right';
+        else Swipes.direction = 'left';
+    }
+
+    static switch_panel() {
+        Swipes.#hide_current_panel();
+        Swipes.#change_panel_number();
+        Swipes.#show_next_panel();
+    }
+
+    static #change_panel_number() {
+        switch(Swipes.direction) {
+            case 'left':
+                Swipes.#panel_number = --Swipes.#panel_number < 0 ? Swipes.#panels.size - 1 : Swipes.#panel_number;
+                break;
+            case 'right':
+                Swipes.#panel_number = ++Swipes.#panel_number % Swipes.#panels.size;
+                break;
         }
     }
 
-    static define_swipe_direction() {
-        Swipes.switching_permitted = false;
-        let scrollableElement = document.querySelector(Swipes.selectors.get('Скролл'));
-        if(scrollableElement.scrollLeft + scrollableElement.clientWidth >= scrollableElement.scrollWidth) 
-            Swipes.permit_right_panel_opening = true;
-        else 
-            Swipes.permit_right_panel_opening = false;
-        if(scrollableElement.scrollLeft === scrollableElement.clientLeft) 
-            Swipes.permit_left_panel_opening = true;
-        else 
-            Swipes.permit_left_panel_opening = false;
+    static #hide_current_panel() {
+        let panel = Swipes.#panels.get(Swipes.#panel_number);
+        panel.style.display = 'none';
     }
 
-    static check_swipe_direction() {
-        let scrollableElement = document.querySelector(Swipes.selectors.get('Скролл'));
-        if(scrollableElement.scrollLeft + scrollableElement.clientWidth >= scrollableElement.scrollWidth) 
-            Swipes.permit_right_panel_opening = true;
-        else 
-            Swipes.permit_right_panel_opening = false;
-        if(scrollableElement.scrollLeft === scrollableElement.clientLeft) 
-            Swipes.permit_left_panel_opening = true;
-        else 
-            Swipes.permit_left_panel_opening = false;
+    static #show_next_panel() {
+        let panel = Swipes.#panels.get(Swipes.#panel_number);
+        panel.style.display = '';
     }
 
-    /** 
-     * Вспомогательные функции
-     */
+    static isRightRange() {
+        let deltaX = Swipes.#to_positive(Swipes.#deltaX);
+        let deltaY = Swipes.#to_positive(Swipes.#deltaY);
+        return (deltaX > 40 && deltaY < 40) ? true : false;
+    }
 
-    static #toPositive(num) {
+    static getPanelEdge() {
+        const {scrollLeft, clientWidth, scrollWidth, clientLeft} = Swipes.main;
+        if(clientWidth !== scrollWidth) {
+            if(scrollLeft === clientLeft) Swipes.panel_edge = 'left';
+            else if(scrollLeft + clientWidth >= scrollWidth) Swipes.panel_edge = 'right';
+            else Swipes.panel_edge = 'none';
+        }
+        else Swipes.panel_edge = 'both';
+    }
+
+    static #to_positive(num) {
         return num < 0 ? -num : num;
-    }
-
-    static #reset_positions() {
-        Swipes.show_permission = false;
-        Swipes.#change_panel_number_permission = true;
-        Swipes.#startX = undefined;
-        Swipes.#startY = undefined;
-        Swipes.#currentX = undefined;
-        Swipes.#currentY = undefined;
-        Swipes.#deltaX = undefined;
-        Swipes.#deltaY = undefined;
     }
 }
 
-document.addEventListener('touchstart', (event) => {
-    Swipes.get_initial_position(event);
-});
-document.addEventListener('touchmove', function(event) {
-    Swipes.get_current_position(event);
-});
-document.addEventListener('touchend', function() {
-    Swipes.define_show_permission();
-    if(Swipes.show_permission) {
-        Swipes.hide_current_panel();
-        Swipes.show_target_panel();
-        Swipes.define_swipe_direction();
+document.addEventListener('touchstart', Swipes.set_initial_coordinates);
+document.addEventListener('touchstart', Swipes.getPanelEdge);
+document.addEventListener('touchmove', Swipes.get_current_coordinates);
+document.addEventListener('touchend', () => {
+    if(Swipes.isRightRange()) {
+        Swipes.define_direction();
+        if(Swipes.panel_edge === 'both') {
+            Swipes.switch_panel();
+        }
+        else if(Swipes.panel_edge === Swipes.direction) {
+            Swipes.switch_panel();
+        }
     }
-    Swipes.switching_permitted = true;
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    let element = document.querySelector(Swipes.selectors.get('Скролл'));
-    element.addEventListener('scroll', Swipes.define_swipe_direction);
-
-    const edgeScrollEvent = new CustomEvent('edgeScroll');
-    element.addEventListener('edgeScroll', () => {
-        Swipes.switching_permitted = true;
-    });
-    element.addEventListener('touchstart', () => {
-        const {scrollLeft, clientWidth, scrollWidth, clientLeft} = element;
-        if(scrollLeft + clientWidth >= scrollWidth - 10)
-            element.dispatchEvent(edgeScrollEvent);
-        if(scrollLeft === clientLeft)
-            element.dispatchEvent(edgeScrollEvent);
-    });
 });
