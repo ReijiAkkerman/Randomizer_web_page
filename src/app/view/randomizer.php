e9eb50297bdbc60a5e4860b0456805f93c669044
ReijiAkkerman <reijiakkerman@gmail.com>
2025-03-05 21:20:00 +0300

    1

--- src/app/view/randomizer.php
+++ src/app/view/randomizer.php
@@ -397,13 +397,19 @@
                             </svg>
                         </button>
                     </div>
-                    <?php for($i = sizeof($data->lists->hard); $i > 0; $i--) { ?>
-                    <div class="lists_select-list" data-type="<?= $data->lists->hard[$i - 1]->type ?>" data-id="<?= $data->lists->hard[$i - 1]->id ?>">
+                    <?php
+                        $lists_ids = [];
+                        foreach($data->lists->main as $list_id => $value) {
+                            $lists_ids[] = $list_id;
+                        }
+                        for($i = sizeof($lists_ids); $i > 0; $i--) { 
+                    ?>
+                    <div class="lists_select-list" data-type="<?= $data->lists->hard[$lists_ids[$i - 1]]->type ?>" data-id="<?= $data->lists->hard[$lists_ids[$i - 1]]->id ?>">
                         <p><?php
-                            if($data->lists->hard[$i - 1]->name) echo $data->lists->hard[$i - 1]->name;
-                            else echo $data->lists->hard[$i - 1]->date;
+                            if($data->lists->hard[$lists_ids[$i - 1]]->name) echo $data->lists->hard[$lists_ids[$i - 1]]->name;
+                            else echo $data->lists->hard[$lists_ids[$i - 1]]->date;
                         ?></p>
-                        <button class="lists_delete-list" data-type="<?= $data->lists->hard[$i - 1]->type ?>" data-id="<?= $data->lists->hard[$i - 1]->id ?>">
+                        <button class="lists_delete-list" data-type="<?= $data->lists->hard[$lists_ids[$i - 1]]->type ?>" data-id="<?= $data->lists->hard[$lists_ids[$i - 1]]->id ?>">
                             <svg viewBox="0 0 1024 1024">
                                 <path d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"/>
                             </svg>
