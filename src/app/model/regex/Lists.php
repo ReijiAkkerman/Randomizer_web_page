0e0401a565765c51ae8bf7038def33f12f4c9693
ReijiAkkerman <reijiakkerman@gmail.com>
2025-03-07 00:42:01 +0300

    1

--- src/app/model/regex/Lists.php
+++ src/app/model/regex/Lists.php
@@ -2,6 +2,6 @@
     namespace project\model\regex;
 
     enum Lists: string {
-        case listName = '#^[А-Яа-яA-Za-z ]{1,250}$#u';
+        case listName = '#^[А-Яа-яA-Za-z0-9 ]{1,250}$#u';
         case languageMark = '#^[a-z]{2}$#';
     }
